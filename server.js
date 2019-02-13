258c18e0045c35d26ba22aaa37712910ba702c5e
Alex Bukhman <alexb823@gmail.com>
2019-02-13 17:30:20 +0000

    server change

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 3000;
 
 syncAndSeed()
   .then(() => {
-    app.listen(port, () => console.log(`Listening on port ${port}`))
+    return app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+      });
     // app.listen(process.env.PORT, process.env.IP, () => console.log('Server has started')) //For when using cloud 9 server
   })
   .catch(error => console.log(error));
